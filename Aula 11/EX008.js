d7e43b5ab7254cfe9c5eda1b21988b4d8a93e742
Marcos Renan Oliveira Silva <renansilvadev08@gmail.com>
2026-03-03 01:03:26 -0300

    Condição simples em JS
    iniciando meus estudos de condições em JS

---
+++ Aula 11/EX008.js
@@ -0,0 +1,7 @@
+var vel = 55
+
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 50) { //condição simples
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+} 
+console.log('se beber não dirija')
