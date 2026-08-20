1baeccdf17401261cc3a8d5a6c339ab3045f59e5
Marcos Renan Oliveira Silva <renansilvadev08@gmail.com>
2026-08-19 22:53:38 -0300

    estruturas de repetição
    introdução as estruturas de repetição

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do{
+    console.log('Olá, mundo!');          //repetição com teste lógico no final
+    c++;
+} while (c <= 6);
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log('Olá, mundo!');          //repetição com teste lógico no início
+    c++;
+}
+*/
+
+/*
+console.log("Olá, mundo!");
+console.log("Olá, mundo!");
+console.log("Olá, mundo!");
+console.log("Olá, mundo!");             //as três formas de escrever a mesma coisa, porém as de cima são mais eficientes
+console.log("Olá, mundo!");
+console.log("Olá, mundo!");
+*/
